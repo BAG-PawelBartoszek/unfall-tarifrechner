b113cd29c3cd0c8e248bb1f5381829c45f37ab9e
pbartoszek <pawel.bartoszek@brockhaus-ag.de>
2025-10-17 13:37:18 +0200

    Sonar

--- unfall-backend/src/main/java/com/pfefferminzia/unfall/UnfallApplication.java
+++ unfall-backend/src/main/java/com/pfefferminzia/unfall/UnfallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnfallApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(UnfallApplication.class, args);
-  }
+    static void main(String[] args) {
+        SpringApplication.run(UnfallApplication.class, args);
+    }
 }
